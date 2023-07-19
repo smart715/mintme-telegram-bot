4181e2630d9b2bd6ba19251ad943e52e1f38b2ab
denlev <levadnyuk66@gmail.com>
2023-07-19 21:23:08 +0300

    Finished CoinGeckoWorker

--- src/core/worker/CoinGeckoWorker.ts
+++ src/core/worker/CoinGeckoWorker.ts
@@ -2,14 +2,16 @@
 import {singleton} from "tsyringe"
 import {AbstractTokenWorker} from "./AbstractTokenWorker";
 import axios from "axios";
+import {TokensService} from "../service";
+import {Blockchain} from "../../utils";
 
 @singleton()
 export class CoinGeckoWorker extends AbstractTokenWorker {
-    public constructor() {
+    public constructor(private readonly tokenService: TokensService) {
         super()
     }
 
-    public async run(currentBlockchain: Crypto): Promise<any> {
+    public async run(currentBlockchain: Blockchain): Promise<any> {
         try {
             console.log(`${CoinGeckoWorker.name} started`)
 
@@ -34,19 +36,72 @@ export class CoinGeckoWorker extends AbstractTokenWorker {
             const data = response.data
             const tokens = data.tokens
 
-            tokens.forEach((token: object) => {
+            for (const token of tokens) {
                 const tokenId: string = token.name.toString().replace(" ", "-")
 
                 if (tokenId.length <= 0 || tokenId.startsWith("realt-")) {
-                    return
+                    continue
                 }
 
                 const bscAddress: string = token.address.toString()
 
                 if (bscAddress < 0) {
-                    return
+                    continue
                 }
-            })
+
+                const coinInfoResponse = await axios.get("https://api.coingecko.com/api/v3/coins/" + tokenId)
+                const coinInfo = coinInfoResponse.data
+
+                if ("" === coinInfo) {
+                    continue
+                }
+
+                const coinName: string = coinInfo.name + "(" + coinInfo.symbol + ")"
+                const links = coinInfo.links
+
+                const website: string = links.homepage
+
+                if (
+                    website.includes("realt.co") ||
+                    coinName.toLowerCase().includes("x short") ||
+                    coinName.toLowerCase().includes("x long") ||
+                    coinName.startsWith("Aave ")
+                ) {
+                    continue
+                }
+
+                let otherLinks = []
+
+                if (links.twitter_screen_name.length > 0) {
+                    otherLinks.push("https://twitter.com/" + links.twitter_screen_name)
+                }
+
+                if (links.facebook_username.length > 0) {
+                    otherLinks.push("https://facebook.com/" + links.facebook_username)
+                }
+
+                if (links.telegram_channel_identifier.length > 0) {
+                    otherLinks.push("https://t.me/" + links.telegram_channel_identifier)
+                }
+
+                if (links.subreddit_url.length > 0) {
+                    otherLinks.push(links.subreddit_url.toString())
+                }
+
+                const allLinks: string[] = otherLinks.concat(links.chat_url)
+
+                await this.tokenService.add(
+                    bscAddress,
+                    coinName,
+                    [website],
+                    [''],
+                    allLinks,
+                    'CoinGecko',
+                    currentBlockchain
+                )
+
+                console.log('Added to DB: ', bscAddress, coinName, website, '', links.join(','), 'CoinGecko', currentBlockchain)
+            }
 
             console.log(`${CoinGeckoWorker.name} finished`)
         } catch (error: any) {
