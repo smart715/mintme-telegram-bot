951ffe88142e90cfe2089d2ebf82648919be1226
denlev <levadnyuk66@gmail.com>
2023-07-19 18:56:04 +0300

    Fetch tokens and use loop

--- src/core/worker/CoinGeckoWorker.ts
+++ src/core/worker/CoinGeckoWorker.ts
@@ -1,6 +1,7 @@
 // @ts-nocheck
 import {singleton} from "tsyringe"
 import {AbstractTokenWorker} from "./AbstractTokenWorker";
+import axios from "axios";
 
 @singleton()
 export class CoinGeckoWorker extends AbstractTokenWorker {
@@ -9,9 +10,48 @@ export class CoinGeckoWorker extends AbstractTokenWorker {
     }
 
     public async run(currentBlockchain: Crypto): Promise<any> {
-        console.log(`${CoinGeckoWorker.name} started`)
+        try {
+            console.log(`${CoinGeckoWorker.name} started`)
 
+            let link: string = ''
 
-        console.log(`${CoinGeckoWorker.name} finished`)
+            switch (currentBlockchain) {
+                case "BSC":
+                    link = "https://tokens.coingecko.com/binance-smart-chain/all.json"
+
+                    break
+                case "ETH":
+                    link = "https://tokens.coingecko.com/ethereum/all.json"
+
+                    break
+                case "CRO":
+                    link = "https://tokens.coingecko.com/cronos/all.json"
+
+                    break
+            }
+
+            const response = await axios.get(link)
+            const data = response.data
+            const tokens = data.tokens
+
+            tokens.forEach((token: object) => {
+                const tokenId: string = token.name.toString().replace(" ", "-")
+
+                if (tokenId.length <= 0 || tokenId.startsWith("realt-")) {
+                    return
+                }
+
+                const bscAddress: string = token.address.toString()
+
+                if (bscAddress < 0) {
+                    return
+                }
+            })
+
+            console.log(`${CoinGeckoWorker.name} finished`)
+        } catch (error: any) {
+            console.log(`${CoinGeckoWorker.name} something went wrong. Reason: ${error.message}`)
+        }
+
     }
 }
