6d5e72150aa839282ad3bc46c83e75b254a67386
denlev <levadnyuk66@gmail.com>
2023-08-07 17:08:48 +0300

    CoinCodex worker, service with api calls, types for CoinCodex api requests

---
+++ src/core/service/parser/CoinCodexService.ts
@@ -0,0 +1,20 @@
+import axios from "axios";
+import { CoinCodexCoinResponse, CoinInfoResponse } from '../../../types'
+
+export class CoinCodexService {
+    public async getAllCoins(): Promise<CoinCodexCoinResponse[]> {
+        const response = await axios.get(
+            'https://coincodex.com/apps/coincodex/cache/all_coins.json'
+        )
+
+        return response.data
+    }
+
+    public async getCoinInfo(tokenId: string): Promise<CoinInfoResponse> {
+        const response = await axios.get(
+            'https://coincodex.com/api/coincodex/get_coin/' + tokenId
+        )
+
+        return response.data
+    }
+}
---
+++ src/core/worker/parser/CoinCodexWorker.ts
@@ -0,0 +1,133 @@
+import { singleton } from 'tsyringe'
+import { AbstractTokenWorker } from '../AbstractTokenWorker'
+import { Blockchain, findContractAddress, logger } from '../../../utils'
+import { CoinCodexService, TokensService } from '../../service'
+import { CoinCodexCoinResponse, CoinInfoResponse } from '../../../types'
+
+@singleton()
+export class CoinCodexWorker extends AbstractTokenWorker {
+    private readonly prefixLog = '[CoinCodex]'
+    private readonly unsupportedBlockchains: Blockchain[] = [ Blockchain.CRO ]
+
+    public constructor(
+        private readonly coinCodexService: CoinCodexService,
+        private readonly tokenService: TokensService,
+    ) {
+        super()
+    }
+
+    public async run(currentBlockchain: Blockchain): Promise<void> {
+        logger.info(`${this.prefixLog} Worker started`)
+
+        if (this.unsupportedBlockchains.includes(currentBlockchain)) {
+            logger.error(`${this.prefixLog} Unsupported blockchain ${currentBlockchain}. Aborting`)
+
+            return
+        }
+
+        const explorerURI = this.getExplorerURI(currentBlockchain)
+        const platformRegExp = this.getPlatformRegEx(currentBlockchain)
+        let i = 0
+
+        let coins: CoinCodexCoinResponse[]
+
+        try {
+            coins = await this.coinCodexService.getAllCoins()
+        } catch (ex: any) {
+            logger.error(
+                `${this.prefixLog} Aborting. Failed to fetch all coins.  Reason: ${ex.message}`
+            )
+
+            return
+        }
+
+        for (const coin of coins) {
+            ++i
+            const tokenId = coin.symbol
+            const coinName = coin.name + '(' + coin.symbol + ')'
+
+            let coinInfo: CoinInfoResponse
+
+            try {
+                coinInfo = await this.coinCodexService.getCoinInfo(tokenId)
+            } catch (ex: any) {
+                logger.error(
+                    `${this.prefixLog} Aborting. Failed to fetch coin info.  Reason: ${ex.message}. Skipping.`
+                )
+
+                continue
+            }
+
+            if (!platformRegExp.test(coinInfo.platform.toLowerCase())) {
+                continue
+            }
+
+            logger.info(`${this.prefixLog} Checking token ${coinName} (${i}/${coins.length})`)
+
+            const website = coinInfo.website
+
+            let contractAddress: string|null = null
+            let links: string[] = []
+
+            coinInfo.socials.forEach((social) => {
+                if (social.value.toLowerCase().includes(explorerURI)) {
+                    contractAddress = findContractAddress(social.value)
+                } else {
+                    links.push(social.value)
+                }
+            })
+
+            if (!contractAddress) {
+                continue
+            }
+
+            const tokenInDb = await this.tokenService.findByAddress(contractAddress, currentBlockchain)
+
+            if (tokenInDb) {
+                continue
+            }
+
+            await this.tokenService.add(
+                contractAddress,
+                coinName,
+                [ website ],
+                [ '' ],
+                links,
+                'CoinCodex',
+                currentBlockchain
+            )
+
+            logger.info(
+                `${this.prefixLog} Added to DB:`,
+                contractAddress,
+                coinName,
+                website,
+                links,
+                'CoinCodex',
+                currentBlockchain
+            )
+        }
+    }
+
+    private getExplorerURI(currentBlockchain: Blockchain): string {
+        switch (currentBlockchain) {
+            case Blockchain.BSC:
+                return 'bscscan'
+            case Blockchain.ETH:
+                return 'etherscan'
+            default:
+                throw new Error('Wrong blockchain provided. Explorer URI doesn\'t exists for provided blockchain')
+        }
+    }
+
+    private getPlatformRegEx(currentBlockchain: Blockchain): RegExp {
+        switch (currentBlockchain) {
+            case Blockchain.BSC:
+                return /binance|bsc/
+            case Blockchain.ETH:
+                return /eth|ethereum/
+            default:
+                throw new Error('Wrong blockchain provided. Platform regex doesn\'t exists for provided blockchain')
+        }
+    }
+}
