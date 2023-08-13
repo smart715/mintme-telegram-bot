dac0c23f51a7ccc010b96030195f73b70f11d2aa
Alexander Kazakov <krytoikazak@mail.ru>
2023-08-13 05:46:58 +0000

    [ts] Config fix

--- config/default.ts
+++ config/default.ts
@@ -1,11 +1,9 @@
-import { Blockchain } from '../src/utils'
-
 export default {
     //Database config
-    'db_host': '{{ db_host }}',
-    'db_user': '{{ db_user }}',
-    'db_password': '{{ db_password }}',
-    'db_name': '{{ db_name }}',
+    'db_host': 'db',
+    'db_user': 'root',
+    'db_password': 'root',
+    'db_name': 'tokens_fetcher',
 
     //CMC
     'coinmarketcap_api_key': '0da9b092-fee6-4c91-875a-e93fbb6b78e0',
@@ -25,8 +23,8 @@ export default {
     'server_ip': '{{ server_ip }}',
 
     explorerApiKeys: {
-        [Blockchain.BSC]: 'M7KD3A8FB4137CMQZ7VV3C696V6WYHD6UE',
-        [Blockchain.ETH]: 'XSYS3JRES31D8P4ZY261FNA5VWTWNZ5XVQ',
-        [Blockchain.CRO]: 'N97BBK59XJ5DK9B8AN87NPEH2DYKBGKHTA',
+        'BSC': 'M7KD3A8FB4137CMQZ7VV3C696V6WYHD6UE',
+        'ETH': 'XSYS3JRES31D8P4ZY261FNA5VWTWNZ5XVQ',
+        'CRO': 'N97BBK59XJ5DK9B8AN87NPEH2DYKBGKHTA',
     },
 }
