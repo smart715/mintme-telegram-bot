99a78e1b70546427c4e84d88baaa4c407a309cf2
denlev <levadnyuk66@gmail.com>
2023-08-16 21:38:03 +0300

    Parse tokens for recent tokens worker

--- src/core/service/parser/RecentTokensService.ts
+++ src/core/service/parser/RecentTokensService.ts
@@ -1,5 +1,5 @@
+import axios from 'axios'
 import { singleton } from 'tsyringe'
-import axios from "axios";
 
 @singleton()
 export class RecentTokensService {
--- src/core/worker/parser/RecentTokensWorker.ts
+++ src/core/worker/parser/RecentTokensWorker.ts
@@ -1,5 +1,6 @@
+import { DOMWindow, JSDOM } from 'jsdom'
 import { AbstractTokenWorker } from '../AbstractTokenWorker'
-import {Blockchain, logger} from '../../../utils'
+import { Blockchain, logger } from '../../../utils'
 import { RecentTokensService, TokensService } from '../../service'
 
 export class RecentTokensWorker extends AbstractTokenWorker {
@@ -21,6 +22,63 @@ export class RecentTokensWorker extends AbstractTokenWorker {
             logger.error(`${this.prefixLog} Unsupported blockchain ${currentBlockchain}. Aborting`)
 
             return
+        }
+
+        let page = 1
+        let tokensCount = 0
+        const targetBlockchain = this.getTargetBlockchain(currentBlockchain)
+
+        do {
+            logger.info(`${this.prefixLog} Page: ${page}`)
+
+            let tokensPageStr: string
+
+            try {
+                tokensPageStr = await this.recentTokensService.getAllTokensPage(targetBlockchain, page)
+            } catch (ex: any) {
+                logger.error(
+                    `${this.prefixLog} Aborting. Failed to get all tokens page. Page: ${page} Reason: ${ex.message}`
+                )
+
+                return
+            }
+
+            const allTokensDOM = (new JSDOM(tokensPageStr)).window
+
+            const tokens  = this.geTokenDivs(allTokensDOM)
+
+            tokensCount = tokens.length
+
+            for (const token of tokens) {
+                const tokenLink = this.getTokenLink(token)
+
+                if (!tokenLink) {
+                    continue
+                }
+            }
+
+            page += 1
+        } while (tokensCount > 0)
+    }
+
+    private getTokenLink(token: Element): HTMLAnchorElement|undefined {
+        return token.getElementsByTagName('a')[0]
+    }
+
+    private geTokenDivs(allTokensDOM: DOMWindow): HTMLCollectionOf<Element> {
+        return allTokensDOM
+            .document
+            .getElementsByClassName('table-coin-info');
+    }
+
+    private getTargetBlockchain(currentBlockchain: Blockchain): string {
+        switch (currentBlockchain) {
+            case Blockchain.ETH:
+                return 'ethereum'
+            case Blockchain.BSC:
+                return 'bsc'
+            default:
+                throw new Error('Wrong blockchain provided. Target blockchain doesn\'t exists for provided blockchain')
         }
     }
 }
