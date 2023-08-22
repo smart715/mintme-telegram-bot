3a0c038e603e31a7d980171281cf21aff3d98b75
denlev <levadnyuk66@gmail.com>
2023-08-22 20:38:18 +0300

    Winston logger with custom filename. Parse messages with metadata and store it in files and disaply in console

--- src/utils/winstonLogger.ts
+++ src/utils/winstonLogger.ts
@@ -1,29 +1,85 @@
-import winston from 'winston';
-import moment from 'moment';
+import winston, { Logger } from 'winston'
+import moment from 'moment'
+import chalk from 'chalk'
 
-export function createLogger(filename: string = 'default') {
-    const logFormat = winston.format.printf(({ timestamp, level, message }) => {
-        const formattedTimestamp = moment(timestamp).format('YYYY-MM-DD HH:mm:ss');
-        return `[${formattedTimestamp}] ${level}: ${message}`;
-    });
+const colors: { [color: string]: typeof chalk.magenta } = {
+    TRACE: chalk.magenta,
+    DEBUG: chalk.cyan,
+    INFO: chalk.blue,
+    WARN: chalk.yellow,
+    ERROR: chalk.red,
+}
 
-    // Function to colorize log levels
-    const colorizeFormat = winston.format.colorize();
+export function createLogger(filename: string = 'default'): Logger {
+    const logFormat = winston.format.printf((info) => {
+        const metaLog = getMetaLog(info)
+
+        const formattedTimestamp = chalk.grey('[' +
+            moment(info.timestamp).format('YYYY-MM-DD HH:mm:ss.SSS')
+        + ']')
+
+        return `${formattedTimestamp} ${colors[info.level.toUpperCase()](info.level)}: ${info.message} ${metaLog}`
+    })
 
     // Create separate transports for console and file
-    const consoleTransport = new winston.transports.Console();
-    const fileTransport = new winston.transports.File({ filename: `var/log/${filename}.log` }); // Default filename
+    const consoleTransport = new winston.transports.Console()
+    const fileTransport = new winston.transports.File({ filename: `var/log/${filename}.log` }) // Default filename
 
     // Create a logger instance
     const winstonLogger = winston.createLogger({
         level: 'info', // Set your desired log level
         format: winston.format.combine(
+            winston.format.json(),
             winston.format.timestamp(),
-            colorizeFormat,
-            logFormat
+            logFormat,
         ),
-        transports: [consoleTransport, fileTransport],
-    });
+        transports: [ consoleTransport, fileTransport ],
+    })
 
     return winstonLogger
+}
+
+function getMetaLog(info: { [key: string|number]: string|object }): string {
+    const keys = Object.keys(info)
+
+    let meta = ''
+
+    for (const key of keys) {
+        if (isNaN(key as any)) {
+            continue
+        }
+
+        const metaParam = info[key]
+        const stillEmpty = '' === meta
+
+        const paramStr = 'string' === typeof metaParam
+            ? metaParam
+            : parseObjectToStr(metaParam)
+
+        meta += stillEmpty
+            ? paramStr
+            : ' ' + paramStr
+    }
+
+    return meta
+}
+
+function parseObjectToStr(metaParam: object): string {
+    const params: string[] = Object.values(metaParam)
+
+    let str: string = '[ '
+
+    for (const paramKey in params) {
+        const lastKey = Number(paramKey) === params.length - 1
+
+        str += chalk.green(params[paramKey])
+
+        if (!lastKey) {
+            str += ', '
+        }
+    }
+
+    str += ' ]'
+
+    return str
 }
