09465d561c62501f1e16bb467a0a250ac72637f4
denlev <levadnyuk66@gmail.com>
2023-08-25 19:47:21 +0300

    TwitterAcount entity and migration

---
+++ migrations/1692976150721-TwitterAccount.ts
@@ -0,0 +1,14 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class TwitterAccount1692976150721 implements MigrationInterface {
+    name = 'TwitterAccount1692976150721'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE TABLE \`twitter_account\` (\`id\` int NOT NULL AUTO_INCREMENT, \`user_name\` varchar(255) NOT NULL, \`assigned_server_ip\` varchar(255) NULL, \`local_storage_json\` text NOT NULL, PRIMARY KEY (\`id\`)) ENGINE=InnoDB`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`DROP TABLE \`twitter_account\``);
+    }
+
+}
---
+++ src/core/entity/TwitterAccount.ts
@@ -0,0 +1,16 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm'
+
+@Entity()
+export class TwitterAccount {
+    @PrimaryGeneratedColumn()
+    public id: number
+
+    @Column()
+    public userName!: string
+
+    @Column({ nullable: true })
+    public assignedServerIP!: string
+
+    @Column('text')
+    public localStorageJson!: string
+}
