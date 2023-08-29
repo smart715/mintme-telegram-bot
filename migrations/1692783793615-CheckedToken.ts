cc88f3f8ad6db423af8294d476c77747c1e7596e
akidse <akidse1@gmail.com>
2023-08-29 17:21:40 +0300

    [ts] migration fix #9486

--- migrations/1692783793615-CheckedToken.ts
+++ migrations/1692783793615-CheckedToken.ts
@@ -19,8 +19,8 @@ export class CheckedToken1692783793615 implements MigrationInterface {
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`DROP INDEX \`UQ_TOKEN_ID_SOURCE\` ON \`parser_checked_token\``)
-        await queryRunner.query(`DROP INDEX \`IDX_SOURCE\` ON \`parser_checked_token\``)
-        await queryRunner.query(`DROP TABLE \`parser_checked_token\``)
+        await queryRunner.query(`DROP INDEX \`UQ_TOKEN_ID_SOURCE\` ON \`checked_token\``)
+        await queryRunner.query(`DROP INDEX \`IDX_SOURCE\` ON \`checked_token\``)
+        await queryRunner.query(`DROP TABLE \`checked_token\``)
     }
 }
