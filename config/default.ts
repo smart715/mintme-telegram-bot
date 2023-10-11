dc87053e91445d49b561dbd988f86ce2597fb1ce
azhari <mazharioffical@gmail.com>
2023-10-11 16:53:12 +0000

    [ts] make config file read the database info from enviroment variables #9665

--- config/default.ts
+++ config/default.ts
@@ -2,10 +2,10 @@ export default {
     'environment': process.env.NODE_ENV,
 
     //Database config
-    'db_host': 'localhost',
-    'db_user': 'root',
-    'db_password': 'root',
-    'db_name': 'marketing_bot',
+    'db_host': process.env.DB_HOST,
+    'db_user': process.env.DB_USER,
+    'db_password': process.env.DB_PASS,
+    'db_name': process.env.DB_NAME,
 
     // Email to send daily statistic.
     // This email receives notifications about broken workers or not available messages/accounts
