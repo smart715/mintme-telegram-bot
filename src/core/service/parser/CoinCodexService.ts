f3e0d37a6e6b9c3c959f2dc08d1e62eb6a290439
Mado <princemado00@gmail.com>
2023-10-21 03:25:29 +0300

    [ts] fix axios incorrect response type on coincodex, #9693

--- src/core/service/parser/CoinCodexService.ts
+++ src/core/service/parser/CoinCodexService.ts
@@ -4,8 +4,15 @@ import { CoinCodexCoinResponse, CoinInfoResponse } from '../../types'
 
 @singleton()
 export class CoinCodexService {
+    private axiosStrict = axios.create({
+        transitional: {
+            silentJSONParsing: false,
+        },
+        responseType: 'json',
+    })
+
     public async getAllCoins(): Promise<CoinCodexCoinResponse[]> {
-        const response = await axios.get(
+        const response = await this.axiosStrict.get(
             'https://coincodex.com/apps/coincodex/cache/all_coins.json'
         )
 
@@ -13,7 +20,7 @@ export class CoinCodexService {
     }
 
     public async getCoinInfo(tokenId: string): Promise<CoinInfoResponse> {
-        const response = await axios.get(
+        const response = await this.axiosStrict.get(
             'https://coincodex.com/api/coincodex/get_coin/' + tokenId
         )
 
