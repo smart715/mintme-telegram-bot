b88aa3f594bc0aa1829b9c18417866cbac41801a
Mado <princemado00@gmail.com>
2023-10-21 03:52:56 +0300

    [ts] add retry in case getAllCoins request takes long time and break from server side, #9693

--- src/core/service/parser/CoinCodexService.ts
+++ src/core/service/parser/CoinCodexService.ts
@@ -1,22 +1,29 @@
-import axios from 'axios'
+import axios, { AxiosInstance } from 'axios'
 import { singleton } from 'tsyringe'
 import { CoinCodexCoinResponse, CoinInfoResponse } from '../../types'
 
 @singleton()
 export class CoinCodexService {
-    private axiosStrict = axios.create({
+    private axiosStrict: AxiosInstance = axios.create({
         transitional: {
             silentJSONParsing: false,
         },
         responseType: 'json',
     })
 
-    public async getAllCoins(): Promise<CoinCodexCoinResponse[]> {
-        const response = await this.axiosStrict.get(
-            'https://coincodex.com/apps/coincodex/cache/all_coins.json'
-        )
+    public async getAllCoins(retries: number = 0): Promise<CoinCodexCoinResponse[]> {
+        try {
+            const response = await this.axiosStrict.get(
+                'https://coincodex.com/apps/coincodex/cache/all_coins.json'
+            )
+            return response.data
+        } catch (error) {
+            if (retries >= 5) {
+                throw error
+            }
 
-        return response.data
+            return this.getAllCoins(retries++)
+        }
     }
 
     public async getCoinInfo(tokenId: string): Promise<CoinInfoResponse> {
