7b8fcf654c72f0e4ce5dcdc8838ef78ce8afff37
mahmoud <mahmoudmoh1499@gmail.com>
2023-11-04 17:44:30 +0000

    [ts] update TokensInsightService to accept many keys

--- src/core/service/parser/TokensInsightService.ts
+++ src/core/service/parser/TokensInsightService.ts
@@ -6,41 +6,72 @@ import { TokensInsightAllCoinsResponse, TokensInsightCoinDataResponse } from '..
 
 @singleton()
 export class TokensInsightService {
-    private readonly apiKey = config.get('tokensinsight_api_key') as string
+    private readonly apiKeys: string[] = config.get('tokensinsight_api_keys') as string[]
+    private currentApiKeyIndex: number = 0
 
     public async getAllCoins(offset: number, limit: number): Promise<TokensInsightAllCoinsResponse> {
-        const headers = {
-            'Content-Type': 'application/json',
-            'TI_API_KEY': this.apiKey,
+        for (let i = 0; i < this.apiKeys.length; i++) {
+            const apiKey = this.apiKeys[this.currentApiKeyIndex]
+            const headers = {
+                'Content-Type': 'application/json',
+                'TI_API_KEY': apiKey,
+            }
+
+            try {
+                const response = await axios.get(
+                    'https://api.tokeninsight.com/api/v1/coins/list',
+                    {
+                        headers: headers,
+                        params: {
+                            offset,
+                            limit,
+                        },
+                    },
+                )
+
+                return response.data
+            } catch (error) {
+                if (i < this.apiKeys.length - 1) {
+                    // If this key fails, move to the next key and retry
+                    this.currentApiKeyIndex = (this.currentApiKeyIndex + 1) % this.apiKeys.length
+                } else {
+                    throw error
+                }
+            }
         }
 
-        const response = await axios.get(
-            'https://api.tokeninsight.com/api/v1/coins/list',
-            {
-                headers: headers,
-                params: {
-                    offset,
-                    limit,
-                },
-            },
-        )
-
-        return response.data
+        // Handle the case where all keys are exhausted or other errors occur
+        throw new Error('All API keys have been exhausted, and the request failed.')
     }
 
     public async getCoinData(id: string): Promise<TokensInsightCoinDataResponse> {
-        const headers = {
-            'Content-Type': 'application/json',
-            'TI_API_KEY': this.apiKey,
+        for (let i = 0; i < this.apiKeys.length; i++) {
+            const apiKey = this.apiKeys[this.currentApiKeyIndex]
+            const headers = {
+                'Content-Type': 'application/json',
+                'TI_API_KEY': apiKey,
+            }
+
+            try {
+                const response = await axios.get(
+                    `https://api.tokeninsight.com/api/v1/coins/${id}`,
+                    {
+                        headers: headers,
+                    },
+                )
+
+                return response.data
+            } catch (error) {
+                if (i < this.apiKeys.length - 1) {
+                    // If this key fails, move to the next key and retry
+                    this.currentApiKeyIndex = (this.currentApiKeyIndex + 1) % this.apiKeys.length
+                } else {
+                    throw error
+                }
+            }
         }
 
-        const response = await axios.get(
-            `https://api.tokeninsight.com/api/v1/coins/${id}`,
-            {
-                headers: headers,
-            },
-        )
-
-        return response.data
+        // Handle the case where all keys are exhausted or other errors occur
+        throw new Error('All API keys have been exhausted, and the request failed.')
     }
 }
