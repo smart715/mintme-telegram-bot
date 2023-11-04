7f7494a36f27a0e55632a285cd2319b33dbae37a
mahmoud <mahmoudmoh1499@gmail.com>
2023-11-04 18:16:27 +0000

    [ts] update CMCWorker to work with many api keys

--- src/core/service/parser/CMCService.ts
+++ src/core/service/parser/CMCService.ts
@@ -5,30 +5,63 @@ import { CMCApiGeneralResponse, CMCCryptocurrency, CMCTokenInfoResponse, CMCWork
 
 @singleton()
 export class CMCService {
-    private apiKey: string = config.get<CMCWorkerConfig>('cmcWorker')['apiKey']
+    private readonly apiKeys: string[] = config.get<CMCWorkerConfig>('cmcWorker')['apiKeys']
+    private currentApiKeyIndex: number = 0
 
     public async getLastTokens(start: number, limit: number): Promise<CMCApiGeneralResponse<CMCCryptocurrency[]>> {
-        const response = await axios.get('https://pro-api.coinmarketcap.com/v1/cryptocurrency/map', {
-            params: {
-                CMC_PRO_API_KEY: this.apiKey,
-                start,
-                limit,
-                sort: 'id',
-            },
-        })
+        for (let i = 0; i < this.apiKeys.length; i++) {
+            const apiKey = this.apiKeys[this.currentApiKeyIndex]
 
-        return response.data
+            try {
+                const response = await axios.get('https://pro-api.coinmarketcap.com/v1/cryptocurrency/map', {
+                    params: {
+                        CMC_PRO_API_KEY: apiKey,
+                        start,
+                        limit,
+                        sort: 'id',
+                    },
+                })
+
+                return response.data
+            } catch (error) {
+                if (i < this.apiKeys.length - 1) {
+                    // If this key fails, move to the next key and retry
+                    this.currentApiKeyIndex = (this.currentApiKeyIndex + 1) % this.apiKeys.length
+                } else {
+                    throw error
+                }
+            }
+        }
+
+        // Handle the case where all keys are exhausted or other errors occur
+        throw new Error('All API keys have been exhausted, and the request failed.')
     }
 
     public async getTokenInfo(slug: string): Promise<CMCApiGeneralResponse<CMCTokenInfoResponse>> {
-        const response = await axios.get('https://pro-api.coinmarketcap.com/v1/cryptocurrency/info', {
-            params: {
-                CMC_PRO_API_KEY: this.apiKey,
-                slug,
-                aux: 'urls,platform',
-            },
-        })
+        for (let i = 0; i < this.apiKeys.length; i++) {
+            const apiKey = this.apiKeys[this.currentApiKeyIndex]
 
-        return response.data
+            try {
+                const response = await axios.get('https://pro-api.coinmarketcap.com/v1/cryptocurrency/info', {
+                    params: {
+                        CMC_PRO_API_KEY: apiKey,
+                        slug,
+                        aux: 'urls,platform',
+                    },
+                })
+
+                return response.data
+            } catch (error) {
+                if (i < this.apiKeys.length - 1) {
+                    // If this key fails, move to the next key and retry
+                    this.currentApiKeyIndex = (this.currentApiKeyIndex + 1) % this.apiKeys.length
+                } else {
+                    throw error
+                }
+            }
+        }
+
+        // Handle the case where all keys are exhausted or other errors occur
+        throw new Error('All API keys have been exhausted, and the request failed.')
     }
 }
