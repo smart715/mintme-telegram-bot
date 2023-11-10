c10cf6af886965214c2cfb7e824e750c08cd9ed1
mahmoud <mahmoudmoh1499@gmail.com>
2023-11-10 17:31:30 +0000

    [ts] retrun bitQuery service to loop

--- src/core/service/parser/BitQueryService.ts
+++ src/core/service/parser/BitQueryService.ts
@@ -6,16 +6,16 @@ import { BitQueryTransfersResponse } from '../../types'
 
 @singleton()
 export class BitQueryService {
-  private readonly apiKeys: string[] = config.get('bitquery_api_keys') as string[]
-  private currentApiKeyIndex: number = 0
+    private readonly apiKeys: string[] = config.get('bitquery_api_keys') as string[]
+    private currentApiKeyIndex: number = 0
 
-  public async getAddresses(offset: number, limit: number, blockchain: string): Promise<BitQueryTransfersResponse> {
-    let retries = this.apiKeys.length
+    public async getAddresses(offset: number, limit: number, blockchain: string): Promise<BitQueryTransfersResponse> {
+        let retries = this.apiKeys.length
 
-    while (retries > 0) {
-      const apiKey = this.apiKeys[this.currentApiKeyIndex]
-      const data = {
-        query: `query ($network: EthereumNetwork!, $limit: Int!, $offset: Int!) {
+        while (retries > 0) {
+            const apiKey = this.apiKeys[this.currentApiKeyIndex]
+            const data = {
+                query: `query ($network: EthereumNetwork!, $limit: Int!, $offset: Int!) {
                     ethereum(network: $network) {
                       transfers(
                         options: {desc: "count", limit: $limit, offset: $offset}
@@ -28,35 +28,35 @@ export class BitQueryService {
                       }
                     }
                   }`,
-        variables: {
-          limit: limit,
-          offset: offset,
-          network: blockchain,
-          dateFormat: '%Y-%m-d',
-        },
-      }
-      const headers = {
-        'Content-Type': 'application/json',
-        'X-API-KEY': apiKey,
-      }
+                variables: {
+                    limit: limit,
+                    offset: offset,
+                    network: blockchain,
+                    dateFormat: '%Y-%m-%d',
+                },
+            }
+            const headers = {
+                'Content-Type': 'application/json',
+                'X-API-KEY': apiKey,
+            }
 
-      try {
-        const response = await axios.post(
-          'https://graphql.bitquery.io',
-          data,
-          { headers }
-        )
+            try {
+                const response = await axios.post(
+                    'https://graphql.bitquery.io',
+                    data,
+                    { headers }
+                )
 
-        return response.data
-      } catch (error) {
-        // If this key fails, move to the next key and retry
-        this.currentApiKeyIndex = (this.currentApiKeyIndex + 1) % this.apiKeys.length
-      }
+                return response.data
+            } catch (error) {
+                // If this key fails, move to the next key and retry
+                this.currentApiKeyIndex = (this.currentApiKeyIndex + 1) % this.apiKeys.length
+            }
 
-      retries--
+            retries--
+        }
+
+        // Handle the case where all keys are exhausted or other errors occur
+        throw new Error('All API keys have been exhausted, and the request failed.')
     }
-
-    // Handle the case where all keys are exhausted or other errors occur
-    throw new Error('All API keys have been exhausted, and the request failed.')
-  }
 }
