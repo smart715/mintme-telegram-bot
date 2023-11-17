fe9838d7c211a653efaa651eb3f11c78a3720f3a
mahmoud <mahmoudmoh1499@gmail.com>
2023-11-17 19:52:39 +0000

    [ts] make BitQueryService use makeRequest fun

--- src/core/service/parser/BitQueryService.ts
+++ src/core/service/parser/BitQueryService.ts
@@ -1,62 +1,55 @@
 /* eslint-disable @typescript-eslint/naming-convention */
-import axios from 'axios'
+import axios, { AxiosInstance } from 'axios'
 import config from 'config'
 import { singleton } from 'tsyringe'
 import { BitQueryTransfersResponse } from '../../types'
+import { makeRequest } from '../ApiService'
 
 @singleton()
 export class BitQueryService {
     private readonly apiKeys: string[] = config.get('bitquery_api_keys') as string[]
-    private currentApiKeyIndex: number = 0
+
+    private readonly axiosInstance: AxiosInstance
+
+    public constructor() {
+        this.axiosInstance = axios.create({
+            baseURL: 'https://graphql.bitquery.io',
+            timeout: 5000,
+        })
+    }
 
     public async getAddresses(offset: number, limit: number, blockchain: string): Promise<BitQueryTransfersResponse> {
-        let retries = this.apiKeys.length
-
-        while (retries > 0) {
-            const apiKey = this.apiKeys[this.currentApiKeyIndex]
-            const data = {
+        const apiParameter = ''
+        const response = await makeRequest<BitQueryTransfersResponse>(this.axiosInstance, apiParameter, {
+            apiKeys: this.apiKeys,
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            params: {
                 query: `query ($network: EthereumNetwork!, $limit: Int!, $offset: Int!) {
-                    ethereum(network: $network) {
-                      transfers(
+                ethereum(network: $network) {
+                    transfers(
                         options: {desc: "count", limit: $limit, offset: $offset}
                         amount: {gt: 0}
-                      ) {
+                    ) {
                         currency {
-                          address
+                            address
                         }
                         count
-                      }
                     }
-                  }`,
+                }
+            }`,
                 variables: {
                     limit: limit,
                     offset: offset,
                     network: blockchain,
                     dateFormat: '%Y-%m-%d',
                 },
-            }
-            const headers = {
-                'Content-Type': 'application/json',
-                'X-API-KEY': apiKey,
-            }
+            } as Record<string, any>,
+        })
 
-            try {
-                const response = await axios.post(
-                    'https://graphql.bitquery.io',
-                    data,
-                    { headers }
-                )
 
-                return response.data
-            } catch (error) {
-                // If this key fails, move to the next key and retry
-                this.currentApiKeyIndex = (this.currentApiKeyIndex + 1) % this.apiKeys.length
-            }
-
-            retries--
-        }
-
-        // Handle the case where all keys are exhausted or other errors occur
-        throw new Error('All API keys have been exhausted, and the request failed.')
+        return response
     }
 }
