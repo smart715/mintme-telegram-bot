ef52832a5df61237624996963e35659690a279f5
Mohammad Azhari <mazharioffical@gmail.com>
2023-11-18 10:23:38 +0200

    [ts] make address unique #9730

---
+++ migrations/1700230896284-MakeTokenUnique.ts
@@ -0,0 +1,14 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class MakeTokenUnique1700230896284 implements MigrationInterface {
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE token DROP INDEX UQ_ADDRESS_BLOCKCHAIN`)
+        await queryRunner.query(`ALTER TABLE token ADD CONSTRAINT UQ_token_address UNIQUE (address)`)
+    }
+    
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE token DROP INDEX UQ_token_address`)
+        await queryRunner.query(`ALTER TABLE token Add UNIQUE index UQ_ADDRESS_BLOCKCHAIN (blockchain, address)`)
+    }
+
+}
