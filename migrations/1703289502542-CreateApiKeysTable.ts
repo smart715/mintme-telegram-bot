1a1ce7e287c0ae37e60da7682f00646653c7c4d0
mahmoud <mahmoudmoh1499@gmail.com>
2023-12-22 21:51:39 +0000

    init api keys table

---
+++ migrations/1703289502542-CreateApiKeysTable.ts
@@ -0,0 +1,23 @@
+import { MigrationInterface, QueryRunner } from 'typeorm'
+
+export class CreateApiKeysTable1703289502542 implements MigrationInterface {
+    public name = 'CreateApiKeysTable1703289502542'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`
+            CREATE TABLE api_key (
+                id INT PRIMARY KEY AUTO_INCREMENT,
+                serviceId INT,
+                api_key VARCHAR(255) NOT NULL,
+                next_attempt_date DATETIME,
+                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+                FOREIGN KEY (serviceId) REFERENCES service(id)
+            );
+        `)
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`DROP TABLE api_key;`)
+    }
+}
---
+++ src/core/entity/ApiKey.ts
@@ -0,0 +1,24 @@
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne } from 'typeorm'
+import { Service } from './Service'
+
+@Entity({ name: 'api_key' })
+export class ApiKey {
+    @PrimaryGeneratedColumn()
+    public id: number
+
+    @ManyToOne(() => Service, service => service.apiKeys)
+    public service: Service
+
+    @Column({ name: 'api_key', length: 255 })
+    public apiKey: string
+
+    @Column({ name: 'next_attempt_date', type: 'datetime', nullable: true })
+    public nextAttemptDate: Date
+
+
+    @Column({ name: 'created_at', type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+    public createdAt: Date
+
+    @Column({ name: 'updated_at', type: 'timestamp', default: () => 'CURRENT_TIMESTAMP', onUpdate: 'CURRENT_TIMESTAMP' })
+    public updatedAt: Date
+}
