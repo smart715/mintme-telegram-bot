06c14660f141d6340288d674e99137a04290a203
mahmoud <mahmoudmoh1499@gmail.com>
2023-12-28 18:28:21 +0000

    [ts] add apiKey repository

---
+++ src/core/repository/ApiKeyRepository.ts
@@ -0,0 +1,20 @@
+// Import necessary modules from TypeORM
+import { Repository, EntityRepository } from 'typeorm'
+import { ApiKey } from '../entity'
+
+@EntityRepository(ApiKey)
+export class ApiKeyRepository extends Repository<ApiKey> {
+    public async findAvailableKey(serviceId: number): Promise<ApiKey | undefined> {
+        return this.findOne({
+            where: {
+                serviceId,
+                nextAttemptDate: null,
+            },
+            order: { updatedAt: 'ASC' },
+        })
+    }
+
+    public async updateNextAttemptDate(apiKeyId: number, nextAttemptDate: Date): Promise<void> {
+        await this.update(apiKeyId, { nextAttemptDate })
+    }
+}
