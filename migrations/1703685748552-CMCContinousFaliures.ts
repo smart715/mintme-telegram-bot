a4c87bdca772d9f56d2b4c13eed4d3d04078b006
Mado <princemado00@gmail.com>
2024-01-03 20:48:58 +0200

    [ts] continous fail migration, #9818

--- migrations/1703685748552-CMCContinousFaliures.ts
+++ migrations/1703685748552-CMCContinousFaliures.ts
@@ -1,18 +1,13 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm'
 
 export class CMCContinousFaliures1703685748552 implements MigrationInterface {
-    name = 'CMCContinousFaliures1703685748552'
+    public name = 'CMCContinousFaliures1703685748552'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE \`coin_market_cap_account\` ADD \`continous_failed\` int NOT NULL DEFAULT '0'`);
-        await queryRunner.query(`ALTER TABLE \`blacklist\` ADD UNIQUE INDEX \`IDX_7ccf86cd47469d27d32bffeeea\` (\`content\`)`);
-        await queryRunner.query(`ALTER TABLE \`queued_contact\` CHANGE \`is_error\` \`is_error\` tinyint NOT NULL`);
+        await queryRunner.query(`ALTER TABLE \`coin_market_cap_account\` ADD \`continous_failed\` int NOT NULL DEFAULT '0'`)
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE \`queued_contact\` CHANGE \`is_error\` \`is_error\` tinyint(1) NULL DEFAULT '0'`);
-        await queryRunner.query(`ALTER TABLE \`blacklist\` DROP INDEX \`IDX_7ccf86cd47469d27d32bffeeea\``);
-        await queryRunner.query(`ALTER TABLE \`coin_market_cap_account\` DROP COLUMN \`continous_failed\``);
+        await queryRunner.query(`ALTER TABLE \`coin_market_cap_account\` DROP COLUMN \`continous_failed\``)
     }
-
 }
