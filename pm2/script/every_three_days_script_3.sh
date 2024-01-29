3d88bac1f50d5f981dff6a973f3e72d3324eaa7f
Mado <princemado00@gmail.com>
2024-01-29 13:11:14 +0200

    seperate every 3 days scripts

---
+++ pm2/script/every_three_days_script_3.sh
@@ -0,0 +1,20 @@
+# Run CoinScope
+npm run cli -- run-fetch-token-worker --name=coin-scope
+
+# Run CoinBuddy
+npm run cli -- run-fetch-token-worker --name=coin-buddy
+
+# Run CoinSniper
+npm run cli -- run-fetch-token-worker --name=coin-sniper
+
+# Run Coinbrain
+npm run cli -- run-fetch-token-worker --name=coin-brain
+
+# Run TokenInsight
+npm run cli -- run-fetch-token-worker --name=tokens-insight
+
+# Run CryptoVoteListWorker
+npm run cli -- run-fetch-token-worker --name=crypto-vote-list
+
+# Run MyCoinVoteWorker
+npm run cli -- run-fetch-token-worker --name=my-coin-vote
