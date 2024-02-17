338751a71baf26f3c31c0231e36869bc9efb389e
mahmoud <mahmoudmoh1499@gmail.com>
2024-02-17 18:03:29 +0000

    use api service class in LastTokenTxDateFetcher t

--- src/core/worker/LastTokenTxDateFetcher.ts
+++ src/core/worker/LastTokenTxDateFetcher.ts
@@ -1,23 +1,13 @@
-import { TokensService } from '../service'
+import { TokensService, ApiService } from '../service'
 import { Blockchain, explorerApiUrls, sleep } from '../../utils'
 import { Token } from '../entity'
 import axios from 'axios'
-import config from 'config'
 import { Logger } from 'winston'
-
-interface ExplorerApiKeysInterface {
-    [Blockchain.BSC]: string[];
-    [Blockchain.ETH]: string[];
-    [Blockchain.CRO]: string[];
-    [Blockchain.MATIC]: string[];
-    [Blockchain.SOL]: string[];
-}
 
 export class LastTokenTxDateFetcher {
     private readonly workerName = LastTokenTxDateFetcher.name
     private readonly noTokensSleepTime = 60 * 1000
     private readonly sleepTimeBetweenTokens = 1000
-    private readonly explorerApiKeys: ExplorerApiKeysInterface = config.get('explorerApiKeys')
     private readonly defaultTxTimestamp = 962083822000
 
     private readonly supportedBlockchains = [
@@ -30,6 +20,7 @@ export class LastTokenTxDateFetcher {
     public constructor(
         private readonly tokensService: TokensService,
         private readonly logger: Logger,
+        private readonly apiService: ApiService,
     ) { }
 
     public async run(): Promise<void> {
@@ -65,27 +56,26 @@ export class LastTokenTxDateFetcher {
     }
 
     private async fetchLastTxTimestamp(tokenAddress: string, blockchain: Blockchain): Promise<number | null> {
-        const apiKeys = this.explorerApiKeys[blockchain]
+        const response = await this.apiService.makeServiceRequests(
+            axios.create({
+                baseURL: `https://${explorerApiUrls[blockchain]}/api`,
+                params: {
+                    module: 'account',
+                    action: 'txlist',
+                    page: 1,
+                    sort: 'desc',
+                    offset: 1,
+                    address: tokenAddress,
+                },
+            }),
+            '',
+            {
+                serviceName: blockchain,
+                method: 'GET',
+            })
 
-        for (let apiKeyIndex = 0; apiKeyIndex < apiKeys.length; apiKeyIndex++) {
-            const apiKey = apiKeys[apiKeyIndex]
-
-            try {
-                const response = await axios.get(this.buildLastTxUrl(tokenAddress, blockchain, apiKey))
-
-                return response.data.result[0] && response.data.result[0].timeStamp
-                    ? parseInt(response.data.result[0].timeStamp) * 1000
-                    : null
-            } catch (error:any) {
-                this.logger.error(`[${this.workerName}] Error with API key ${apiKey}: ${error.message}`)
-            }
-        }
-
-        throw new Error(`[${this.workerName}] All API keys have been exhausted, and the request failed.`)
-    }
-
-    private buildLastTxUrl(tokenAddress: string, blockchain: Blockchain, apiKey: string): string {
-        const explorerApiUrl = explorerApiUrls[blockchain]
-        return `https://${explorerApiUrl}/api?apikey=${apiKey}&module=account&action=txlist&page=1&sort=desc&offset=1&address=${tokenAddress}`
+        return response.data.result[0] && response.data.result[0].timeStamp
+            ? parseInt(response.data.result[0].timeStamp) * 1000
+            : null
     }
 }
