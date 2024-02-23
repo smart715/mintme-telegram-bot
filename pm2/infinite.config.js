487e99b304b7c565db8d92ae4e7f06956ff4f221
mado <princemado00@gmail.com>
2024-02-23 19:53:09 +0000

    update pm2 scripts

--- pm2/infinite.config.js
+++ pm2/infinite.config.js
@@ -2,9 +2,33 @@
 module.exports = {
     apps: [
         {
-            name: 'token-holding',
+            name: 'token-holding-bsc',
             script: 'npm',
-            args: 'run cli -- run-explorer-worker --name=token-holdings-worker',
+            args: 'run cli -- run-explorer-worker --name=token-holdings-worker  --blockchain=BSC',
+            autorestart: true,
+        },
+        {
+            name: 'token-holding-eth',
+            script: 'npm',
+            args: 'run cli -- run-explorer-worker --name=token-holdings-worker  --blockchain=ETH',
+            autorestart: true,
+        },
+        {
+            name: 'token-holding-cro',
+            script: 'npm',
+            args: 'run cli -- run-explorer-worker --name=token-holdings-worker  --blockchain=CRO',
+            autorestart: true,
+        },
+        {
+            name: 'token-holding-matic',
+            script: 'npm',
+            args: 'run cli -- run-explorer-worker --name=token-holdings-worker  --blockchain=MATIC',
+            autorestart: true,
+        },
+        {
+            name: 'token-holding-sol',
+            script: 'npm',
+            args: 'run cli -- run-explorer-worker --name=token-holdings-worker  --blockchain=SOL',
             autorestart: true,
         },
         {
@@ -20,9 +44,33 @@ module.exports = {
             autorestart: true,
         },
         {
-            name: 'token-transactions-fetcher',
+            name: 'token-transactions-fetcher-bsc',
             script: 'npm',
-            args: 'run cli -- run-explorer-worker --name=token-transactions-fetcher',
+            args: 'run cli -- run-explorer-worker --name=token-transactions-fetcher --blockchain=BSC',
+            autorestart: true,
+        },
+        {
+            name: 'token-transactions-fetcher-eth',
+            script: 'npm',
+            args: 'run cli -- run-explorer-worker --name=token-transactions-fetcher --blockchain=ETH',
+            autorestart: true,
+        },
+        {
+            name: 'token-transactions-fetcher-cro',
+            script: 'npm',
+            args: 'run cli -- run-explorer-worker --name=token-transactions-fetcher --blockchain=CRO',
+            autorestart: true,
+        },
+        {
+            name: 'token-transactions-fetcher-matic',
+            script: 'npm',
+            args: 'run cli -- run-explorer-worker --name=token-transactions-fetcher --blockchain=MATIC',
+            autorestart: true,
+        },
+        {
+            name: 'token-transactions-fetcher-sol',
+            script: 'npm',
+            args: 'run cli -- run-explorer-worker --name=token-transactions-fetcher --blockchain=SOL',
             autorestart: true,
         },
         {
