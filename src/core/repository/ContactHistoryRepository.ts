1e72c1213d69a360e48b2da0da1fd2a066beb8a0
Mado <princemado00@gmail.com>
2024-05-30 19:05:13 +0300

    [ts] use day stats instead of 24h for accounts

--- src/core/repository/ContactHistoryRepository.ts
+++ src/core/repository/ContactHistoryRepository.ts
@@ -2,35 +2,34 @@ import { singleton } from 'tsyringe'
 import { Between, EntityRepository, Repository } from 'typeorm'
 import { ContactHistory, TwitterAccount } from '../entity'
 import { GroupedContactsCount } from '../types'
+import moment from 'moment'
 
 @singleton()
 @EntityRepository(ContactHistory)
 export class ContactHistoryRepository extends Repository<ContactHistory> {
     public async getCountSentTwitterMessagesDaily(twitterAccount: TwitterAccount): Promise<number> {
-        const now = new Date()
-        const oneDayAgo = new Date()
-        oneDayAgo.setHours(now.getHours() - 24)
+        const now = moment().utc()
+        const startOfDay = moment().utc().subtract(1, 'day').endOf('day')
 
         return this
             .count({
                 where: {
                     twitterAccountId: twitterAccount.id,
                     isSuccess: true,
-                    createdAt: Between(oneDayAgo, now),
+                    createdAt: Between(startOfDay.toDate(), now.toDate()),
                 },
             })
     }
 
     public async getCountAttemptsTwitterDaily(twitterAccount: TwitterAccount): Promise<number> {
-        const now = new Date()
-        const oneDayAgo = new Date()
-        oneDayAgo.setHours(now.getHours() - 24)
+        const now = moment().utc()
+        const startOfDay = moment().utc().subtract(1, 'day').endOf('day')
 
         return this
             .count({
                 where: {
                     twitterAccountId: twitterAccount.id,
-                    createdAt: Between(oneDayAgo, now),
+                    createdAt: Between(startOfDay.toDate(), now.toDate()),
                 },
             })
     }
